f615b695e0418c0170e280f4f784fc7c2e9e0b43
Sebastian Tapia <sebastiantapiadlt@gmail.com>
2018-10-17 11:21:10 -0500

    Corregir ingreso Usuarios y agregar confirmacion

--- IngresoDatos.php
+++ IngresoDatos.php
@@ -17,13 +17,14 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM usuarios";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
-    $sql = "INSERT INTO usuarios(usuarios, clave, correo) VALUES ('$usuario', '$clave','$correo'";
+    $sql = "INSERT INTO usuarios(usuarios, clave, correo) VALUES ('$usuario', '$clave','$correo')";
 
 } else {
     echo "0 results";
 }
 
 if ($conn->query($sql) === TRUE) {
+    echo "Usuario registrado correctamente";
     
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
