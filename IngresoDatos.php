e341b2caf01c74883af20fbc1c74e5c82e156efa
Sebastian Tapia <sebastiantapiadlt@gmail.com>
2018-10-18 11:06:02 -0500

    cambio de nombres/eliminadoRegistroOriginal

--- IngresoDatos.php
+++ IngresoDatos.php
@@ -3,33 +3,27 @@ $servername = "localhost";
 $username = "stapiadlt";
 $password = "b27x1242";
 $dbname = "prograweb";
-$usuario = $_GET["usuario"];
-$clave = $_GET["clave"];
-$correo = $_GET["correo"];
+$usuario = $_REQUEST["usuario"];
+$clave = $_REQUEST["clave"];
+$correo = $_REQUEST["correo"];
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 $sql = "SELECT * FROM usuarios";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     $sql = "INSERT INTO usuarios(usuarios, clave, correo) VALUES ('$usuario', '$clave','$correo')";
-
 } else {
     echo "0 results";
 }
-
 if ($conn->query($sql) === TRUE) {
     echo "Usuario registrado correctamente";
-    
+
 } else {
     echo "Error: " . $conn->error;
 }
-
-
 $conn->close();
 ?>
